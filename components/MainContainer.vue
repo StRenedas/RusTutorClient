27cff6d8642c8bfc7c5d26d44ba86af0f55b132e
Stanislav Sosedov <55929542+StRenedas@users.noreply.github.com>
2021-04-04 00:55:51 +0500

    Main page is almost done

---
+++ components/MainContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main-container">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.main-container {
+    height: 800px;
+    background-color: #D7E0F5;
+}
+</style>
