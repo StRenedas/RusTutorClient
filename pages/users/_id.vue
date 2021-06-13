c231fc8f7b78ce56b821772f9206a33ea13e366a
Stanislav Sosedov <55929542+StRenedas@users.noreply.github.com>
2021-06-13 15:22:00 +0500

    fix

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -16,7 +16,7 @@ import { mapGetters, mapMutations } from "vuex";
 
 export default {
   async asyncData({params, $axios}) {
-    let res = await $axios.$post('https://rustutor-backend.herokuapp.com/stats', {userid: `${params.id}`});
+    let res = await $axios.$post('https://rustutor-backend.herokuapp.com/errors', {userid: `${params.id}`});
     return {res};
   },
   middleware: 'auth',
